5b5b7b37b999a4adcfdd0070e3518c6e72bbc147
YOSHIOKA Takuma <lo48576@hard-wi.red>
2021-06-26 16:21:11 +0900

    Add basic files

---
+++ src/main.rs
@@ -0,0 +1,9 @@
+//! burne: BUlk ReName by Editor.
+#![forbid(unsafe_code)]
+#![warn(rust_2018_idioms)]
+// `clippy::missing_docs_in_private_items` implies `missing_docs`.
+#![warn(clippy::missing_docs_in_private_items)]
+#![warn(clippy::unwrap_used)]
+
+/// Entrypoint.
+fn main() {}
